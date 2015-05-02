0ee1c3294c7749c72ebc40947bcf31bb8049eba6
Jeffrey Biles <bilesjeffrey@gmail.com>
2015-05-02 14:03:36 -0500

    everything but the basics commented out

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // ctx: Ember.computed(function(){
+  //   let canvas = document.getElementById("myCanvas");
+  //   return canvas.getContext("2d");
+  // }),
+  // x: 100,
+  // y: 100,
+
   didInsertElement: function(){
     this.drawCircle()
   },
@@ -18,4 +25,17 @@ export default Ember.Component.extend({
     ctx.closePath()
     ctx.fill() 
   },
+
+  // mainLoop: function(){
+  //   var ctx = this.get('ctx'),
+  //   x = this.get('x'),
+  //   y = this.get('y');
+
+  //   ctx.fillStyle = '#aaa';
+  //   ctx.clearRect(0, 0, 400, 300)
+  //   this.drawCircle()
+  //   this.set('x', x + 1);
+  //   this.set('y', y + 1);
+  //   Ember.run.later(this, this.mainLoop, 1000/60)
+  // }
 });
