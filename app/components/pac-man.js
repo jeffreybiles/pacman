a8f8b6b3f1d0bd1285ca8dddc2bc364ff1c3237d
jeffreybiles <bilesjeffrey@gmail.com>
2015-05-05 23:56:29 -0600

    circle moves.  It moves!

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -1,41 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // ctx: Ember.computed(function(){
-  //   let canvas = document.getElementById("myCanvas");
-  //   return canvas.getContext("2d");
-  // }),
-  // x: 100,
-  // y: 100,
+  ctx: Ember.computed(function(){
+    let canvas = document.getElementById("myCanvas");
+    return canvas.getContext("2d");
+  }),
+  x: 100,
+  y: 100,
+
 
   didInsertElement: function(){
-    this.drawCircle()
+    this.mainLoop()
   },
 
   drawCircle: function(){
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
-    let x = 50;
-    let y = 100;
     let radius = 20;
 
     ctx.fillStyle = '#000'
     ctx.beginPath()
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.arc(this.get('x'), this.get('y'), radius, 0, Math.PI * 2, false);
     ctx.closePath()
     ctx.fill() 
   },
 
-  // mainLoop: function(){
-  //   var ctx = this.get('ctx'),
-  //   x = this.get('x'),
-  //   y = this.get('y');
+  mainLoop: function(){
+    var ctx = this.get('ctx');
 
-  //   ctx.fillStyle = '#aaa';
-  //   ctx.clearRect(0, 0, 400, 300)
-  //   this.drawCircle()
-  //   this.set('x', x + 1);
-  //   this.set('y', y + 1);
-  //   Ember.run.later(this, this.mainLoop, 1000/60)
-  // }
+    ctx.fillStyle = '#aaa';
+    ctx.clearRect(0, 0, 800, 600)
+    this.drawCircle()
+    this.set('x', this.get('x') + 1);
+    this.set('y', this.get('y') + 1);
+    Ember.run.later(this, this.mainLoop, 1000/60)
+  }
 });
