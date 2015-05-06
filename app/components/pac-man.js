8a49980f3a30af5cd7fe0ed1465eab491b1c8a8a
jeffreybiles <bilesjeffrey@gmail.com>
2015-05-06 10:38:51 -0600

    collision detection with the outside walls

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -12,6 +12,7 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   dy: 1,
   boardWidth: 800,
   boardHeight: 600,
+  radius: 20,
 
 
   didInsertElement: function(){
@@ -21,11 +22,10 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   drawCircle: function(){
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
-    let radius = 20;
 
     ctx.fillStyle = '#000'
     ctx.beginPath()
-    ctx.arc(this.get('x'), this.get('y'), radius, 0, Math.PI * 2, false);
+    ctx.arc(this.get('x'), this.get('y'), this.get('radius'), 0, Math.PI * 2, false);
     ctx.closePath()
     ctx.fill() 
   },
@@ -41,10 +41,36 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   },
 
   calculatePacMovement: function(){
+    this.collisionDetection();
     this.set('x', this.get('x') + this.get('dx'));
     this.set('y', this.get('y') + this.get('dy'));
   },
 
+  collisionDetection: function(){
+    if(this.get('x') - this.get('radius') <= 0){
+      this.collide('left');
+    } else if(this.get('x') + this.get('radius') >= this.get('boardWidth')){
+      this.collide('right');
+    } else if(this.get('y') - this.get('radius') <= 0){
+      this.collide('top');
+    } else if (this.get('y') + this.get('radius') >= this.get('boardHeight')){
+      this.collide('bottom');
+    }
+  },
+
+  collide: function(direction){
+    this.set('dx', 0);
+    this.set('dy', 0);
+    if(direction === 'left') {
+      this.set('x', this.get('x') + 1);
+    } else if(direction === 'right'){
+      this.set('x', this.get('x') - 1);
+    } else if(direction === 'top'){
+      this.set('y', this.get('y') + 1);
+    } else if(direction === 'bottom'){
+      this.set('y', this.get('y') - 1);
+    }
+  },
 
   keyboardShortcuts: {
     up: function(){
